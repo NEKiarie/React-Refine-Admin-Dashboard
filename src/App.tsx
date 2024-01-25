7c6f0b3fda398807afe0a061d19e400d02ddb600
NEKiarie <s49188705@gmail.com>
2024-01-25 15:58:01 +0300

    :sparkles:Graphql websocket connection feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,71 @@
 import {
-    Refine,
-    GitHubBanner,
-    WelcomePage,
-    Authenticated,
-} from '@refinedev/core';
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import {
-    AuthPage, ErrorComponent
-    , useNotificationProvider
-    , ThemedLayoutV2
-    , ThemedSiderV2
-} from '@refinedev/antd';
+  AuthPage,
+  ErrorComponent,
+  useNotificationProvider,
+  ThemedLayoutV2,
+  ThemedSiderV2,
+} from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, { GraphQLClient, liveProvider } from "@refinedev/nestjs-query";
+import { dataProvider, liveProvider } from "./providers";
+
 import { createClient } from "graphql-ws";
-import { App as AntdApp } from "antd"
+import { App as AntdApp } from "antd";
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier, DocumentTitleHandler } from "@refinedev/react-router-v6";
+import routerBindings, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
 
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { ForgotPassword } from "./pages/forgotPassword";
 
-
-
-
-
 function App() {
-
-    return (
-        <BrowserRouter>
-            <GitHubBanner />
-            <RefineKbarProvider>
-                <AntdApp>
-                    <DevtoolsProvider>
-                        <Refine 
-                           //dataProvider={dataProvider(gqlClient)}
-                            // liveProvider={liveProvider(wsClient)}
-                            notificationProvider={useNotificationProvider}
-                            routerProvider={routerBindings}
-                            // authProvider={}
-                            options={{
-                                syncWithLocation: true,
-                                warnWhenUnsavedChanges: true,
-                                useNewQueryKeys: true,
-                                projectId: "btYeQI-ygTWqj-94rNbG",
-                                liveMode: "auto",
-                            }}
-                        >
-
-
-                            <Routes>
-                                <Route index element={<WelcomePage />} />
-                            </Routes>
-                            <RefineKbar />
-                            <UnsavedChangesNotifier />
-                            <DocumentTitleHandler />
-                        </Refine>
-                        <DevtoolsPanel />
-                    </DevtoolsProvider>
-                </AntdApp>
-
-            </RefineKbarProvider>
-        </BrowserRouter>
-    );
-};
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              // authProvider={}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "btYeQI-ygTWqj-94rNbG",
+                liveMode: "auto",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,15 +1,46 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, {
+  GraphQLClient,
+  liveProvider as graphqlLiveProvider,
+} from "@refinedev/nestjs-query";
+
 import { fetchWrapper } from "./fetch-wrapper";
+import { createClient } from "graphql-ws";
 
-
-export const API_URL = "https://api.crm.refine.dev"
+export const API_BASE_URL = "https://api.crm.refine.dev";
+export const API_URL = "https://api.crm.refine.dev";
+export const WS_URl = "wss://api.crm.refine.dev/graphql";
 
 export const client = new GraphQLClient(API_URL, {
-    fetch: (url: string, options: RequestInit) => {
-        try {
-         return fetchWrapper(url, options)
-        } catch (error) {
-          return Promise.reject(error as Error)
-        }
+  fetch: (url: string, options: RequestInit) => {
+    try {
+      return fetchWrapper(url, options);
+    } catch (error) {
+      return Promise.reject(error as Error);
     }
-})
+  },
+});
+
+//WebSocket connection to listen to Subscriptions
+export const wsClient =
+  typeof window !== "undefined"
+    ? createClient({
+        url: WS_URl,
+        connectionParams: () => {
+          const accessToken = localStorage.getItem("access_token");
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
+
+//Data Provider to make requests to GraphQl API
+export const dataProvider = graphqlDataProvider(client);
+
+//Live Provider to make requests to GraphQl API
+export const liveProvider = wsClient
+  ? graphqlLiveProvider(wsClient)
+  : undefined;
